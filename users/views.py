44bb463c8f841946a8e1687bf7bb4d512742a8c6
maka70vv <25.makarovv@gmail.com>
2024-02-29 15:11:22 +0600

    INS-20 fixing registration

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 
 from .models import User
 from .serializers import UserSerializer
@@ -6,5 +7,6 @@ from .serializers import UserSerializer
 
 # Create your views here.
 class UsersViewSet(viewsets.ModelViewSet):
+    permission_classes = [AllowAny]
     queryset = User.objects.all()
     serializer_class = UserSerializer
