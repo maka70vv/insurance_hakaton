b1178b9e851a0c6b6a11f6b72cc849b5e5ebf84b
maka70vv <25.makarovv@gmail.com>
2024-03-02 13:19:01 +0600

    selling policies urls

--- policies/urls.py
+++ policies/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from policies import views
 
 urlpatterns = [
-    path('accidient/create/', views.AccidentPolicyCreateAPIViewWithCommission.as_view(), name=''),
-    path('dms/create/', views.DMSPolicyCreateAPIViewWithCommission.as_view(), name='')
+    path('accidient/create/platform/', views.AccidentPolicyCreateAPIViewWithCommission.as_view(), name=''),
+    path('cars/create/platform/', views.CarPolicyCreateAPIViewWithCommission.as_view(), name=''),
+    path('cargos/create/platform/', views.CargoPolicyCreateAPIViewWithCommission.as_view(), name=''),
+    path('vzr/create/platform/', views.VZRPolicyCreateAPIViewWithCommission.as_view(), name=''),
+    # path('dms/create/', views.DMSPolicyCreateAPIViewWithCommission.as_view(), name='')
 ]
